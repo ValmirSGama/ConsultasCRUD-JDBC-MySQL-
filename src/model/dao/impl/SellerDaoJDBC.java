1d6259d891d83f275e8ec6d328adf44d4d5a7ead
Valmir Sales <valmir_salesgama@hotmail.com>
2024-07-24 09:26:16 -0300

    delete implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -92,10 +92,23 @@ public class SellerDaoJDBC implements SellerDao {
 		}
 	}
 
+	// Implementação do método para deletar vendedores e seus dados
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+			
+			st.setInt(1, id);
+			
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	// Implementação do método para retornar um vendedor por "ID"
